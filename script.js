765e326524324d00d8abe6befc8ff40a86f05e99
BigD2027 <bigd2027@hotmail.com>
2026-02-12 18:51:29 -0500

    Initial site upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll(".cta");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    alert("This is a mockup. Replace this button with your real payment or application link.");
+  });
+});
